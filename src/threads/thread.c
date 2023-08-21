be3d5359e03bcdaa310894b1bb242bc0f893e806
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-21 22:56:14 +0900

    feat(thread): add parent thread initialization

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -697,6 +697,17 @@ init_thread (struct thread *t, const char *name, int priority)
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
+
+#ifdef USERPROG
+  /* Set parent thread. */
+  struct thread *cur;
+  if (t != initial_thread)
+    cur = thread_current ();
+  else
+    cur = NULL; /* initial_thread does not have parent. */
+  t->parent = cur;
+  list_init (&t->children_ctx_list);
+#endif
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
