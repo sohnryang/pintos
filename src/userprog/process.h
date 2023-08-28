23d0ff84002be2c5d5f7f602578620ed4aa3136c
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-28 23:43:51 +0900

    fix(process): add missing function defs
    This happens when you write bunch of code and commit it into pieces...

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -13,6 +13,9 @@ void process_activate (void);
 void process_cleanup_ctx (struct process_context *);
 void process_trigger_exit (int) NO_RETURN;
 
+struct fd_context *process_create_fd_ctx (void);
+void process_remove_fd_ctx (struct fd_context *);
+
 struct process_context *process_child_ctx_by_pid (tid_t);
 struct fd_context *process_get_fd_ctx (int);
 
