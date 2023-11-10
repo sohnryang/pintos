583e8f3df793caa1e03a34c715f8a0cc594444e8
Ryang Sohn <loop.infinitely@gmail.com>
2023-11-11 00:55:54 +0900

    refactor(fault): use functions from vmm

--- src/vm/fault.c
+++ src/vm/fault.c
@@ -1,12 +1,10 @@
 #include "vm/fault.h"
 
 #include "lib/debug.h"
-#include "threads/malloc.h"
 #include "threads/palloc.h"
-#include "threads/thread.h"
 #include "threads/vaddr.h"
-#include "userprog/pagedir.h"
 #include "vm/frame.h"
+#include "vm/vmm.h"
 
 #include <hash.h>
 #include <stdbool.h>
@@ -18,27 +16,17 @@ fault_handle_not_present (void *fault_addr)
 {
   void *kpage, *upage;
   struct frame *frame;
-  struct thread *cur;
-  bool writable;
-
-  cur = thread_current ();
 
   kpage = palloc_get_page (PAL_USER);
   if (kpage == NULL) // TODO: implement swapping
     return false;
 
-  frame = malloc (sizeof (struct frame));
-  if (frame == NULL) // TODO: implement swapping
-    return false;
-
-  frame_init (frame, kpage);
-
   upage = pg_round_down (fault_addr);
-  frame_add_upage_mapping (frame, upage);
-  hash_insert (&cur->frames, &frame->elem);
-
-  writable = pagedir_is_writable (cur->pagedir, upage);
-  pagedir_set_page (cur->pagedir, upage, kpage, writable);
+  frame = vmm_lookup_frame (upage);
+  if (frame == NULL)
+    return false;
+  if (!vmm_deserialize_frame (frame, kpage))
+    return false;
 
   return true;
 }
